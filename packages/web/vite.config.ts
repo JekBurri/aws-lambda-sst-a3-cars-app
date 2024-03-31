7d11adb3bae76eed32af6261528b46a7cd8a3761
JekBurri <onorbtanko@gmail.com>
2024-03-31 12:13:14 -0700

    Add proxy configuration for API requests

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      '/api': {
+        target: 'https://93gd5xa9m8.execute-api.us-west-2.amazonaws.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
