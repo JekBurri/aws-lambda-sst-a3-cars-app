c9352d09d14ae41559d00ac40d3dbfd14d364fcb
JekBurri <onorbtanko@gmail.com>
2024-03-31 14:14:26 -0700

    Add Home component to App.tsx

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Cars from "../components/Cars.tsx";
 import Landing from "../components/Landing.tsx";
+import Home from "../components/Home.tsx"
 import Header from "../components/Header.tsx";
 import Search from "../components/Search.tsx"
 import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
